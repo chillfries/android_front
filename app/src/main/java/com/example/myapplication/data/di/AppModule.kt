cf278013cf36da316cc5131d62690ebf7133c2b1
gamja <bear356se@gmail.com>
2025-10-12 15:34:12 +0900

    냉장고 탭 데이터베이스 구현

--- app/src/main/java/com/example/myapplication/data/di/AppModule.kt
+++ app/src/main/java/com/example/myapplication/data/di/AppModule.kt
@@ -1,5 +1,9 @@
 package com.example.myapplication.data.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.myapplication.data.AppDatabase
+import com.example.myapplication.data.FridgeDao
 import com.example.myapplication.data.repository.AuthRepositoryImpl
 import com.example.myapplication.data.repository.CameraRepositoryImpl
 import com.example.myapplication.data.repository.FridgeRepositoryImpl
@@ -9,6 +13,7 @@ import com.example.myapplication.domain.repository.FridgeRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -16,6 +21,24 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    // --- Database & DAO ---
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "my_fridge_app.db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFridgeDao(appDatabase: AppDatabase): FridgeDao {
+        return appDatabase.fridgeDao()
+    }
+
+    // --- Repositories ---
     @Provides
     @Singleton
     fun provideAuthRepository(): AuthRepository {
@@ -28,9 +51,10 @@ object AppModule {
         return CameraRepositoryImpl()
     }
 
+    // FridgeRepositoryImpl을 FridgeRepository의 구현체로 제공한다고 Hilt에 명시합니다.
     @Provides
     @Singleton
-    fun provideFridgeRepository(): FridgeRepository {
-        return FridgeRepositoryImpl()
+    fun provideFridgeRepository(fridgeDao: FridgeDao): FridgeRepository {
+        return FridgeRepositoryImpl(fridgeDao)
     }
 }
